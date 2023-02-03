fafe8b1f5707d0c186d33cc876f331ed5b616101
patildeep07 <patildeep07@gmail.com>
2023-02-04 00:44:13 +0530

    Responsive navigation bar

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+// Sticky Header
+
 const header = document.querySelector("header")
 const sectionOne = document.querySelector(".landingPage")
 
@@ -16,3 +19,21 @@ const sectionOneObserver = new IntersectionObserver (function (entries, sectionO
 }, sectionOneOptions)
 
 sectionOneObserver.observe(sectionOne)
+
+
+// Mobile Menu
+
+const primaryNav = document.querySelector(".primary-nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible')
+
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', "true")
+        navToggle.setAttribute('aria-expanded', "true")
+    } else {
+        primaryNav.setAttribute('data-visible', "false" )
+        navToggle.setAttribute('aria-expanded', "false")
+    }
+} )
