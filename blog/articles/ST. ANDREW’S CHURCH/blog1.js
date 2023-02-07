d814039df089a022ce7eabf40ad9af3a98bdb63a
patildeep07 <patildeep07@gmail.com>
2023-02-07 20:00:29 +0530

    header updated on article page

--- blog/articles/ST. ANDREW’S CHURCH/blog1.js
+++ blog/articles/ST. ANDREW’S CHURCH/blog1.js
@@ -1,17 +1,18 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
 
-// Get the header
-var header = document.getElementById("myHeader");
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+// Mobile Menu
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+const primaryNav = document.querySelector(".primary-nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible')
+
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', "true")
+        navToggle.setAttribute('aria-expanded', "true")
+    } else {
+        primaryNav.setAttribute('data-visible', "false" )
+        navToggle.setAttribute('aria-expanded', "false")
+    }
+} )
