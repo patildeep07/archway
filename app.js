6f01cf36bac2d8cd35839bb53c878aaf66fe4655
patildeep07 <patildeep07@gmail.com>
2023-03-02 23:14:22 +0530

    home - gallery

--- app.js
+++ app.js
@@ -37,3 +37,26 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', "false")
     }
 } )
+
+
+// Initialize Swiper
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
--- gallery/gallery.js
+++ gallery/gallery.js
@@ -22,10 +22,11 @@ navToggle.addEventListener('click', () => {
 var swiper = new Swiper(".mySwiper", {
     loop: true,
     spaceBetween: 10,
-    slidesPerView: 4,
+    slidesPerView: 3,
     freeMode: true,
     watchSlidesProgress: true,
   });
+
   var swiper2 = new Swiper(".mySwiper2", {
     loop: true,
     spaceBetween: 10,
